3eddecd3b1911e1d63a24a14361b1d07b167fe4b
kkk232 <kir.zay1999@gmail.com>
2025-02-20 14:06:25 +0300

    Add script.js. Add design. Add clear and delete button. Fix bug.

---
+++ script.js
@@ -0,0 +1,111 @@
+ 
+const container = document.querySelector("#container");
+const content = document.createElement("div");
+let string = "";
+const btnMin = document.querySelector("#btnMin");
+const btnFin = document.querySelector("#btnFin");
+const btnCl = document.querySelector("#clear");
+
+function del(){
+    if(string.trimEnd().split(" ").length == 2) string = string.substring(0, string.length - 3);
+    else string = string.substring(0, string.length - 1)
+    content.textContent = string;
+    container.appendChild(content);
+}
+
+function addDigit(a){
+    if (string[0] == " ") string = "";
+    string += a;
+    content.textContent = string;
+    container.appendChild(content);
+}
+
+function addDot(){
+    if (string[0] == " ") string = "";
+    let str = string.split(" ");
+    if ((str.length > 2 && str[2].includes(".")) || (str.length < 2 && str[0].includes(".")))
+        {
+            content.textContent = string;
+            container.appendChild(content);
+        }
+        else {
+            string += ".";
+            content.textContent = string;
+            container.appendChild(content);
+        }
+}
+function addOperators(a){
+    if (string[0] == " ") string = string.trimStart();
+    if (string.length != 0 && string[string.length - 1] != "-" && string.includes(" ") == false){
+        a == 0 ? string += " + ": a == 1 ? string += " / ": string += " * "
+        content.textContent = string;
+        container.appendChild(content);
+    }
+}
+
+btnCl.addEventListener("click", () =>{
+    string = "";
+    content.textContent = string;
+    container.appendChild(content);
+})
+
+btnMin.addEventListener("click", () => {
+    if (string[0] == " ") string = string.trimStart();
+    if(string.length == 0 || (string.includes(" ") && string[string.length - 1] == " ")){
+        string += "-";
+        content.textContent = string;
+        container.appendChild(content);
+    }
+    else if((string.includes(" ") && string[string.length - 1] !== " ") || string[string.length - 1] == "-" || (string.length == 1 && string.includes(".")) || (string.includes(" ") && string[string.length - 1] == ".")){
+        content.textContent = string;
+        container.appendChild(content);
+    }
+    else {
+        string += " - ";
+        content.textContent = string;
+        container.appendChild(content);
+    }
+    });
+btnFin.addEventListener("click", () => {
+    if (string[0] == " ") string = string.trimStart();
+    if (string.includes(" ") && (string[string.length - 1] != " " || string[string.length - 1] != "-")){
+        let a = 0;
+        str = string.split(" ");
+        switch (str[1]){
+            case "+":
+                a = +str[0] + +str[2];
+                string += ` = ${a}`;
+                break;
+            case "-":
+                a = +str[0] - +str[2];
+                string += ` = ${a}`;
+                break;
+            case "/":
+                a = +str[0] / +str[2];
+                string += ` = ${a}`;
+                break;
+            case "*":
+                a = +str[0] * +str[2];
+                string += ` = ${a}`;
+                break;
+        }
+        content.textContent = string;
+        container.appendChild(content);
+        string = ` ${a}`;
+    }
+    
+});
+
+document.addEventListener("keydown", (e) => {
+    if (e.key === "Enter") {
+        btnFin.dispatchEvent(new Event("click"));
+        return;
+    };
+    const buttons = document.querySelectorAll(".button");
+    buttons.forEach(button => {
+        if (e.key === button.dataset.key) {
+            button.dispatchEvent(new Event("click"));
+            return;
+        };
+    });
+});
